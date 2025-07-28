f7a55a7294fe0a4f2d06a98674e792fe390ad170
Hugo De Freitas Evangelista <hugo.evangelista@cwi.com.br>
2025-07-28 20:39:09 -0300

    Adicionando configs

---
+++ src/main/java/com/unileste/sisges/config/DataSourceConfig.java
@@ -0,0 +1,32 @@
+package com.unileste.sisges.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+
+import javax.sql.DataSource;
+import java.net.URI;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${DATABASE_URL}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws Exception {
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(url)
+                .username(username)
+                .password(password)
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
